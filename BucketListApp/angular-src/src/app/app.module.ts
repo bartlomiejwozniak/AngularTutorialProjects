c8be9279a1c7ad36513f34ddc6086614dc5c2282
Bartek <bartekwozniak93@gmail.com>
2018-05-16 23:23:59 +0200

    Add AddList and ViewList components. Add List service

--- BucketListApp/angular-src/src/app/app.module.ts
+++ BucketListApp/angular-src/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { HttpModule } from '@angular/http';
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { AddListComponent } from './add-list/add-list.component';
+import { ViewListComponent } from './view-list/view-list.component';
+
+import { ListService } from './services/list.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddListComponent,
+    ViewListComponent
   ],
+  //Modules go here
   imports: [
     BrowserModule
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  //All the services go here
+  providers: [ListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
