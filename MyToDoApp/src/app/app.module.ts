0f1fe9ab61ebfd3eec357dca8aef9fcf322bdf1e
Bartek <bartekwozniak93@gmail.com>
2018-05-22 21:12:43 +0200

    Add dashboard and addnewtask components. Attach onsubmit method

--- MyToDoApp/src/app/app.module.ts
+++ MyToDoApp/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AddNewTaskComponent } from './add-new-task/add-new-task.component';
+import { MyDashBoardComponent } from './my-dash-board/my-dash-board.component';
+import {HttpModule} from "../../../BucketListApp/angular-src/node_modules/@angular/http";
+import {FormsModule} from "../../../BucketListApp/angular-src/node_modules/@angular/forms";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddNewTaskComponent,
+    MyDashBoardComponent
   ],
+  //Modules go here
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ MyToDoApp/src/app/my-dash-board/my-dash-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-dash-board',
+  templateUrl: './my-dash-board.component.html',
+  styleUrls: ['./my-dash-board.component.css']
+})
+export class MyDashBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
